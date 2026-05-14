0f11fbaddbf987bafd36b9b3b730a93ae6b217b0
gabriel <offsilvagabriel@gmail.com>
2026-05-14 08:41:37 -0300

    cadastro notas + faltas

--- cadastro-notas.ts
+++ cadastro-notas.ts
@@ -6,15 +6,18 @@ const nomeAluno:string = readline.question('Informe seu nome: ')
 const nota1:number = Number(readline.question('Informe a primeira nota: '))
 const nota2:number = Number(readline.question('Informe a segunda nota: '))
 const nota3:number = Number(readline.question('Informe a terceira nota: '))
+const numeroFaltas:number = Number(readline.question('Informe a quanditade de faltas: '))
 let situacao:string = ''
 
-const media = (nota1 + nota2 + nota3) / 3;
+const media = (nota1 + nota2 + nota3) / 3
 
-if (media >= 7) {
-   situacao = 'Aprovado'
+if (media >= 7 && numeroFaltas < 5) {
+  situacao = 'Aprovado'
+} else if (media >= 5 && media < 7 && numeroFaltas < 3) {
+  situacao = 'Recuperação'
 } else { 
-   situacao = 'Reprovado'
+  situacao = 'Reprovado'
 }
 
-console.log(`Olá, ${nomeAluno}! Sua média é de ${media.toFixed(1)} e sua situação é de ${situacao}`)
+console.log(`Olá, ${nomeAluno}! Sua média é de ${media.toFixed(1)}! e um total de ${numeroFaltas} faltas e sua situação é de ${situacao}!`)
 	
